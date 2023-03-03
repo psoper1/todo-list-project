f9dad1c3302be68c6d696a7c38a3a56079e4a7aa
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-03 00:00:55 +0000

    got checkbot working

--- app/src/components/ListItems.js
+++ app/src/components/ListItems.js
@@ -1,15 +1,30 @@
-function ListItems({ notes, setNotes }) {
-    function removeItem(noteid) {
-        let newArray = notes.filter((potato) => potato.id !== noteid);
-        setNotes(newArray);
-        console.log(newArray)
-    }
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
-    // let notesArray = notes.map(note => <li id="listItem" key={note.text}><input type="checkbox" /> {note.text}<button onClick={removeItem}>-</button></li>)
-    return (
-      <ul>
-        {notes.map(note => <li key={note.text}><input type="checkbox" /> {note.text}<button onClick={() => removeItem(note.id)}>-</button></li>)}
-      </ul>
-    )
+function ListItems({ notes, setNotes }) {
+  function removeItem(noteid) {
+    let newArray = notes.filter((potato) => potato.id !== noteid);
+    setNotes(newArray);
+    console.log(newArray)
   }
+
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = event => {
+
+    if (event.target.checked) {
+      console.log('Checked');
+
+    } else {
+      console.log('not checked');
+    }
+    setChecked(current => !current);
+  }
+
+  return (
+    <ul>
+      {notes.map(note => <li className={`${checked ? "text-decoration-line-through" : ""}`} key={note.text}><input value={checked} onChange={handleChange} type="checkbox" /> {note.text}<button onClick={() => removeItem(note.id)}>-</button></li>)}
+    </ul>
+  )
+}
 export default ListItems;
